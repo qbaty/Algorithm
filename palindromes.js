df0fe6991727849db1072af6467fc7175892ad56
qbaty <qbaty.qi@gmail.com>
2014-02-18 15:04:58 +0800

    Update palindromes.js
    fixed early bug

--- palindromes.js
+++ palindromes.js
@@ -5,14 +5,26 @@
  * then the possible palindromes= a, b, b, a, bb, abba 
  * So the result is 6.
  **/
+ 
+function isSymmetry(str){
+	var n = 0, l = str.length; m = Math.ceil(l/2);
+	for(n; n < m; n++){
+		if(str.charAt(n) == str.charAt(l-n-1)){
+			continue;
+		}else{
+			return false;
+		}
+	}
+	return true;
+}
 
 function palindromes(str){
 	var l = str.length;
 	var result = [];
 	for(var i = 0; i < l; i++){
-		for(var j = 0; j < l-i-1; j++){
-			if(str.charAt(j) == str.charAt(j + i)){
-				result.push(str.substr(j, i+1));
+		for(var j = l-i-1; j > 0; j--){
+			if(isSymmetry(str.substr(i, j+1))){
+				result.push(str.substr(i, j+1));
 			}
 		}
 	}
