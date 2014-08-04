427df02c0da36be64b80eb0d57edd87d66f056c7
qbaty <qbaty.qi@gmail.com>
2014-08-04 12:15:37 +0800

    Update and rename getNthIntersectionLargest.js to getNthLargestIntersection.js

--- Array/getNthLargestIntersection.js
+++ Array/getNthLargestIntersection.js
@@ -3,9 +3,9 @@
  * try to find out the kth greatest common factor
  * var arrA = [1, 3, 6, 12, 24, 34, 88, 102]
  * var arrB = [2, 6, 2, 4, 24, 45, 88, 123]
- * getIntersectionLargest(arrA, arrB, 1) == 88;
+ * getNthLargestIntersection(arrA, arrB, 1) == 88;
 **/
-function getIntersectionLargest(arrA, arrB, n){
+function getNthLargestIntersection(arrA, arrB, n){
 	var c = 0, i = 0;
 	var temp;
 	arrA.sort(function(a,b){return b-a});
